bd251795c1a6727960556df04bbe6235a3184f67
ziolechka <olvas.dev@gmail.com>
2025-06-14 23:44:16 +0300

    add tf files

---
+++ main.tf
@@ -0,0 +1,61 @@
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu-latest.id
+  instance_type          = var.server_size
+  vpc_security_group_ids = [aws_security_group.web.id]
+  user_data              = <<EOF
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>${var.server_name}-WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+systemctl start apache2
+systemctl enable apache2
+EOF
+
+  tags = {
+    Name  = "${var.server_name}-WebServer"
+    Owner = "Olga Sili"
+  }
+}
+
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
+resource "aws_security_group" "web" {
+  name_prefix = "${var.server_name}-WebServer-SG"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "${var.server_name}-WebServer SecurityGroup"
+    Owner = "Olga Sili"
+  }
+}
+
+resource "aws_eip" "web" {
+ domain    = "vpc" # Need to add in new AWS Provider version
+  instance = aws_instance.web.id
+  tags = {
+    Name  = "${var.server_name}-WebServer-IP"
+    Owner = "Olga Sili"
+  }
+}
